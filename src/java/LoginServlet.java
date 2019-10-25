09cc8765460c197121eab246e0c9b82ad657da53
Aluno20 <Aluno20@DESKTOP-D0E0A9S>
2019-10-25 20:39:47 -0300

    Adicionei arquivo no Web-inf para o GIT reconhecer a pasta

--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -39,7 +39,7 @@ public class LoginServlet extends HttpServlet {
             out.println("<title>Servlet LoginServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet LoginServlet at " + request.getParameter("email")+ "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
